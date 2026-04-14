5289da03debc60f58360bc70e804fab94c8e3b1c
mohamed_bnz <simobnz81@gmail.com>
2026-04-14 16:18:11 +0100

    ceartion de table users avec son champs

--- structure.sql
+++ structure.sql
@@ -4,3 +4,12 @@ CREATE TABLE roles (
     id int AUTO_INCREMENT PRIMARY KEY ,
     lable TEXT
 );
+CREATE TABLE users(
+    id int AUTO_INCREMENT PRIMARY KEY ,
+    fierstname VARCHAR(20) ,
+    lastname VARCHAR(20),
+    email VARCHAR(20) UNIQUE ,
+    password VARCHAR(20) ,
+    role_id int,
+    Foreign Key (role_id) REFERENCES roles(id)
+);
