b8b3cc45e80750303e020454dc3a128d2f4601c9
mohamed_bnz <simobnz81@gmail.com>
2026-04-16 10:26:07 +0100

    insertion de la table users

--- seed.sql
+++ seed.sql
@@ -1,3 +1,7 @@
 INSERT INTO roles(lable)
 VALUES('admin'),('prof'),('student');
 
+INSERT INTO users(fierstname,lastname,email,password,role_id)
+VALUES('mohamed','bnz','medbnz@gmail.com','bnzbnz',1),
+('ali','asmarani','ali@gmail.com','aliali',2),
+('anass','elali','anass@gmail.com','anass',3);
