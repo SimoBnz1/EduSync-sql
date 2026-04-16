20eefee0efa2ac0e7ff6d5ce73316f854a47f61b
mohamed_bnz <simobnz81@gmail.com>
2026-04-16 10:29:36 +0100

    insertion de la table classes

--- seed.sql
+++ seed.sql
@@ -5,3 +5,8 @@ INSERT INTO users(fierstname,lastname,email,password,role_id)
 VALUES('mohamed','bnz','medbnz@gmail.com','bnzbnz',1),
 ('ali','asmarani','ali@gmail.com','aliali',2),
 ('anass','elali','anass@gmail.com','anass',3);
+
+INSERT INTO classes(name,classroom_number)
+VALUES('Développeur Web 2026',20),
+('Data Science 2026',16),
+('Réseau 2026',14);
